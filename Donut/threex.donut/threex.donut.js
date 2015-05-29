6c8f74dacd50073f8acd85921b2b33806907774f
AdlerPagliarini <adlerpagliarini@gmail.com>
2015-05-28 23:40:11 -0300

    Revert "Confete"
    This reverts commit 10b485caa92ff54bc27dd4ef06ac1637913efce8.

--- Donut/threex.donut/threex.donut.js
+++ Donut/threex.donut/threex.donut.js
@@ -27,50 +27,58 @@ THREEx.Donut.loadSpaceFighter03	= function(onLoad){
 //////////////////////////////////////////////////////////////////////////////////
 
 THREEx.Donut.Shoot	= function(){
-	//CylinderGeometry(radiusTop, radiusBottom, height, radiusSegments, heightSegments, openEnded, thetaStart, thetaLength)
-/*radiusTop — Radius of the cylinder at the top. Default is 20.
-radiusBottom — Radius of the cylinder at the bottom. Default is 20.
-height — Height of the cylinder. Default is 100.
+	// your code goes here
+	var canvas	= generateShootCanvas();
+	var texture	= new THREE.Texture( canvas );
+	texture.needsUpdate = true;
 
-radiusSegments — Number of segmented faces around the circumference of the cylinder. Default is 8
-heightSegments — Number of rows of faces along the height of the cylinder. Default is 1.
-
-openEnded — A Boolean indicating whether the ends of the cylinder are open or capped. Default is false, meaning capped.
-thetaStart — Start angle for first segment, default = 0 (three o'clock position).
-thetaLength — The central angle, often called theta, of the circular sector. The default is 2*Pi, which makes for a complete cylinder.*/
-	var geometry = new THREE.CylinderGeometry( 0.015, 0.001, 0.15, 8);
-
-	//geometry.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 1 / 2, 0 ) );
-	geometry.applyMatrix( new THREE.Matrix4().makeRotationZ( Math.PI / 2 ) );
-	var material	= new THREE.MeshPhongMaterial({
-/*		map		: THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'images/earthmap1k.jpg'),
-		bumpMap		: THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'images/earthbump1k.jpg'),
-		bumpScale	: 0.05,
-		specularMap	: THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'images/earthspec1k.jpg'),*/
-		color		: color() * 0xffffff,
-		specular	: new THREE.Color('black')
-	})
-	var mesh = new THREE.Mesh(geometry, material);
-
-	return mesh	
-	/* laiser
-	
-		var baseUrl	= THREEx.Donut.baseUrl;
-		var url		= baseUrl + 'images/lensflare0_alpha.png';
-		var texture	= THREE.ImageUtils.loadTexture(url);
-		color		: 0x00ffff,
+	// do the material	
+	var material	= new THREE.MeshBasicMaterial({
+		color		: 0xffaacc,
 		map		: texture,
 		side		: THREE.DoubleSide,
 		blending	: THREE.AdditiveBlending,
-		opacity		: 2,
 		depthWrite	: false,
-		transparent	: true,
-		specular	: new THREE.Color('blue'),*/
+		transparent	: true
+	})
+
+	var container	= new THREE.Object3D
+	container.scale.multiplyScalar(1/2)
+	var nPlanes	= 4;
+	for(var i = 0; i < nPlanes; i++){
+		var geometry	= new THREE.PlaneGeometry(1,1)
+		var mesh	= new THREE.Mesh(geometry, material)
+		mesh.material	= material
+		mesh.rotation.x	= i*Math.PI/nPlanes
+		container.add(mesh)
+	}
+	
+	return container	
+		
+	function generateShootCanvas(){
+		// init canvas
+		var canvas	= document.createElement( 'canvas' );
+		var context	= canvas.getContext( '2d' );
+		canvas.width	= 16;
+		canvas.height	= 74;
+		// set gradient
+		var gradient	= context.createRadialGradient(
+			canvas.width/2, canvas.height /2, 0,
+			canvas.width/2, canvas.height /2, canvas.width /2
+		);		
+		gradient.addColorStop( 0  , 'rgba(255,255,255,1)' );
+		gradient.addColorStop( 0.5, 'rgba(192,192,192,1)' );
+		gradient.addColorStop( 0.8, 'rgba(128,128,128,0.7)' );
+		gradient.addColorStop( 1  , 'rgba(0,0,0,0)' );
+
+		// fill the rectangle
+		context.fillStyle	= gradient;
+		context.fillRect(0,0, canvas.width, canvas.height);
+		// return the just built canvas 
+		return canvas;	
+	};
 }
-function color(){
-	var c = ['0xff0084','0xff0084','0x00ffea', '0x8a00ff'];//'0x8aff00'
-	return c[Math.floor((Math.random() * 3) + 1)];
-}
+
 /**
  * create a detonation effect. 
  */
--- Donut/threex.planets/threex.planets.js
+++ Donut/threex.planets/threex.planets.js
@@ -7,13 +7,13 @@ THREEx.Planets.baseURL	= '../'
 
 
 THREEx.Planets.createEarth	= function(){
-	var geometry	= new THREE.SphereGeometry(0.12, 32, 32)
+	var geometry	= new THREE.SphereGeometry(0.1, 32, 32)
 	var material	= new THREE.MeshPhongMaterial({
 		map		: THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'images/earthmap1k.jpg'),
 		bumpMap		: THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'images/earthbump1k.jpg'),
-		bumpScale	: 0.03,
+		bumpScale	: 0.05,
 		specularMap	: THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'images/earthspec1k.jpg'),
-		specular	: new THREE.Color('black'),
+		specular	: new THREE.Color('grey'),
 	})
 	var mesh	= new THREE.Mesh(geometry, material)
 	return mesh	
